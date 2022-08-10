8a12ac3e97c1afc149a3261364c6312789d00e49
sarah fulham <sarahlfulham@gmail.com>
2022-08-10 15:11:26 -0400

    restaurant controller

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,6 +2,6 @@ class Restaurant < ApplicationRecord
   has_many :reviews, dependent: :destroy
   CATEGORIES = %w[chinese italian japanese french belgian]
 
-  validates :name, :address, :category, presence: true
+  validates :name, :address, :category, :phone_number, presence: true
   validates :category, inclusion: { in: CATEGORIES }
 end
